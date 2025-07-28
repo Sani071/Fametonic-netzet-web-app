27bae990f0e40295aaabfe5cbfd76252f32f3ba2
sani071 <saniahmed071@gmail.com>
2025-07-28 12:08:25 +0600

    refactor: Remove unnecessary 'md:px-0' class from HomePage layout for cleaner styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import FeatureSection from "@/components/sections/homePage/FeatureSection";
 export default function HomePage() {
   return (
     <section className={`w-full bg-black text-white relative overflow-hidden`}>
-      <div className="max-w-5xl mx-auto px-5 md:px-0 lg:pt-15">
+      <div className="max-w-5xl mx-auto px-5 lg:pt-15">
         
         {/* Simple 2-Section Grid with Order Control - 50/50 Split */}
         <div className="grid grid-cols-1 lg:grid-cols-2 relative">
